6fe78dcd30ce289021c546b8b9a0a15d570d9ce5
Nazife Atmaca <nazifeatmaca622@gmail.com>
2024-06-30 17:48:59 +0300

    Çin zodyağı uygulaması tamamlanmıştır.

---
+++ src/CinZodyagiHesaplama.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class CinZodyagiHesaplama {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Doğum yılını giriniz:");
+        int year=scanner.nextInt();
+
+        int mod=year%12;
+        String burc="";
+        switch (mod){
+            case 0:
+                burc="Maymun";
+                break;
+            case 1:
+                burc="Horoz";
+                break;
+            case 2:
+                burc="Köpek";
+                break;
+            case 3:
+                burc="Domuz";
+                break;
+            case 4:
+                burc="Fare";
+                break;
+            case 5:
+                burc="Öküz";
+                break;
+            case 6:
+                burc="Kaplan";
+                break;
+            case 7:
+                burc="Tavşan";
+                break;
+            case 8:
+                burc="Ejderha";
+                break;
+            case 9:
+                burc="Yılan";
+                break;
+            case 10:
+                burc="At";
+                break;
+            case 11:
+                burc="Koyun";
+                break;
+            default:
+                break;
+
+        }
+        System.out.println("Çin Zodyağı Burcunuz: "+burc);
+    }
+}
