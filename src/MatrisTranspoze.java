c64d46b4be7b7d3c752a2156423aa572055cced4
Nazife Atmaca <nazifeatmaca622@gmail.com>
2024-07-07 23:06:57 +0300

    Matris Transpoze uygulaması tamamlanmıştır.

--- src/MatrisTranspoze.java
+++ src/MatrisTranspoze.java
@@ -1,10 +1,8 @@
-import java.util.Arrays;
-
 public class MatrisTranspoze {
     public static void main(String[] args) {
         int[][] list={{1, 2, 3},
                       {4, 5, 6}};
-       
+
 
         int[][] duplicate=new int[list[0].length][list.length];
         for (int i=0;i<list.length;i++){
